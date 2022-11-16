427f94f34369c9f5e9545a5b075b5cca50d4dd27
lsantarcangelo <lsantarcangelo@yahoo.com.ar>
2022-11-15 21:49:09 -0300

    se sube index.ejs y carpeta partials con header.ejs y footer.ejs

--- app.js
+++ app.js
@@ -3,29 +3,32 @@ const app = express();
 const path = require('path');
 const PORT = 3000
 
+//Motor de vistas
+app.set('view engine', 'ejs'); 
+
 //Levantando servidor
 app.listen(PORT, () => console.log('Servidor corriendo en el puerto' + PORT));
 
 //Rutas
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.render(path.join(__dirname, '/views/index'))
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/login.html'))
+    res.render(path.join(__dirname, '/views/login'))
 });
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'))
+    res.render(path.join(__dirname, '/views/register'))
 });
 
 app.get('/shopping-cart', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/views/shoping-cart.html'))
-})
+    res.render(path.join(__dirname, '/views/shoping-cart'))
+});
 
 app.get('/detalle', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/views/detalle-producto.html'))
-})
+    res.render(path.join(__dirname, '/views/product-detail'))
+});
 
 //Recursos estaticos
 app.use(express.static('public'));
