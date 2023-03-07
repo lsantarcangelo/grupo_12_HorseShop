1e8ed919140cba17766ce41abd8805ba4b0bc6ba
aleSer308 <alegresergio@hotmail.com.ar>
2023-03-07 01:17:32 -0300

    Correccion apiUserControllers

--- src/controllers/api/apiUsersControllers.js
+++ src/controllers/api/apiUsersControllers.js
@@ -24,37 +24,42 @@ const apiUsersControlers={
                     status: 200,
                     url: 'api/users/profile/id'
                 },
-                data: user
+                data: [
+                    user.firstname,
+                    user.lastname,
+                    user.email, 
+                    user.user_img
+                ]
             }
             res.json(respuesta)
         })
     },
-    create: (req, res) => {
-        User.create({
-            firstname: req.body.firstName,
-			lastname: req.body.lastName,
-			email: req.body.email,
-			password: req.body.password, 
-			user_img: req.body.image, 
-			created_at: req.body.created_at,
-			updated_at: req.body.updated_at,
-        })
-        .then(confirm =>{
-            if(confirm){
-                response = {
-                    meta:{
-                        satatus: 200,
-                        total : confirm.length,
-                        url : '/api/user/create',
-                    },
+    // create: (req, res) => {
+    //     User.create({
+    //         firstname: req.body.firstName,
+	// 		lastname: req.body.lastName,
+	// 		email: req.body.email,
+	// 		password: req.body.password, 
+	// 		user_img: req.body.image, 
+	// 		created_at: req.body.created_at,
+	// 		updated_at: req.body.updated_at,
+    //     })
+    //     .then(confirm =>{
+    //         if(confirm){
+    //             response = {
+    //                 meta:{
+    //                     satatus: 200,
+    //                     total : confirm.length,
+    //                     url : '/api/user/create',
+    //                 },
                     
-                    data:confirm
-                }
-            }
-            res.json(response)
-        })
-        .catch(err => res.send(err))
-    }
+    //                 data:confirm
+    //             }
+    //         }
+    //         res.json(response)
+    //     })
+    //     .catch(err => res.send(err))
+    // }
 }
 
 module.exports = apiUsersControlers;
